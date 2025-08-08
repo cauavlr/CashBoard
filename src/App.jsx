519c243a50eb7313ae947923c2df14e65630783a
cauavlr <carolcaua2018@gmail.com>
2025-08-08 17:40:56 -0300

    temporario exclu- bankAPI.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { bankAPI, importBankTransactions } from "./services/bankAPI.js";
 
 export default function Dashboard() {
   // Estados para dados bancários
